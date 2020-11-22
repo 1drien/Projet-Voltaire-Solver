d95b365d8882422b7dab08ebcb5463638bf5c5bb
Kim Jung Hun <74764309+AlexRAZU95@users.noreply.github.com>
2020-11-23 00:52:41 +0100

    1.1.0 uplaud
    Meilleur performance, l'algo de trie amélioré
    interface plus simple, plus besoin de coller, le logiciel récupère de lui-même dans le presse papier et test sont contenus

--- VoltaireTaMere/VoltaireTaMere.py
+++ VoltaireTaMere/VoltaireTaMere.py
@@ -28,32 +28,21 @@ def data_Extract (PATH):
     print(PATH)
     Fichier_ERR = open( PATH ,"r", encoding="utf-8") 
     data_Brut = Fichier_ERR.read()+"€"
-    data_Brut = data_Brut[data_Brut.index("[\"java.util.ArrayList"):data_Brut.index("]€")] + "]€"
-
-    pos = 0
-    str_Build = ""
-    is_Build = False
+    data_Brut = data_Brut[data_Brut.index("[\"java.util.ArrayList"):data_Brut.index("€")]
     data_Clear = []
+    i = 0
+    extract = "a"
+    while extract != "":
+        extract = data_Brut[ data_Brut.find("\"",i): data_Brut.find("\"", i+data_Brut.find("\"")+1) ]
+        if "\\x3C" in extract:
+            data_Clear += [extract.replace("\"","").replace("\\x3CB\\x3E", "< ").replace("\\x3C/B\\x3E", " >").replace("\\x27","'").replace("\\xA0"," ").replace("\\x26#x2011;","-")]
 
-    while data_Brut[pos] != "€":
-        if is_Build == True and data_Brut[pos] == "\"":
-            is_Build = False
-            data_Clear += [str_Build]
-            str_Build = ""
-
-        if is_Build == False and data_Brut[pos] == "\"":
-            is_Build = True
-            
-        if is_Build == True:
-            str_Build += data_Brut[pos]
-            
-        pos += 1
+        i += len(extract)
 
     Fichier_ERR.close()
     return data_Clear
 
 def test_Str():
-    boiteOutPut.tag_config("err",foreground = "#DC143C")
     global data
 
     if data == []:
@@ -62,25 +51,30 @@ def test_Str():
         return 0
     
     print(boitePhrase.get(1.0,END))
-    Close_Match = difflib.get_close_matches("\""+boitePhrase.get(1.0,END).replace("-","\\x26#x2011;").replace("'","\\x27")+"\"", data)
+    Close_Match = difflib.get_close_matches(boitePhrase.get(1.0,END), data)
     print(Close_Match)
     if Close_Match != []:
         for i in range(len(Close_Match)):
-            if "\\x3E" in Close_Match[i]:
-                
+            if "<" in Close_Match[i]:
                 boiteOutPut.delete(1.0,"end")
-                boiteOutPut.insert(1.0,Close_Match[i].replace("\\x3CB\\x3E", "< ").replace("\\x3C/B\\x3E", " >").replace("\\x27","'").replace("\\xA0"," ").replace("\\x26#x2011;","-")+"\"","err")
+                boiteOutPut.insert(1.0,Close_Match[i],"err")
                 return 0
 
     boiteOutPut.delete(1.0,"end")
     boiteOutPut.insert(1.0,"il n'y a pas de faute(s)","noerr")
     return 0
 
+def auto_Work():
+    if boitePhrase.get(1.0,"end").replace("\n","") != root.clipboard_get().replace("\n","") and "//OK" not in root.clipboard_get() :
+        boitePhrase.delete(1.0,"end")
+        boitePhrase.insert(1.0,root.clipboard_get().replace("\n",""))
+        test_Str()
+    root.after(500,auto_Work)
+
 def load_Fill():
     global data
     data = []
     boiteOutPut.delete(1.0,"end")
-    boiteOutPut.tag_config("noerr",foreground = "#8eb912")
 
     if extra_PATH.get() != "":
         data = data_Extract(extra_PATH.get())
@@ -88,7 +82,7 @@ def load_Fill():
         if ListBModule.curselection() == ():
             ListBModule.selection_set(0)
 
-        if len(modules.get()) == 121:
+        if len(modules.get()) == 135:
             data = data_Extract(".\Modules\\Supérieur\\Module" + str(ListBModule.curselection()[0] + 1 ) + ".txt")
         else:
             data = data_Extract(".\Modules\\Excellence\\Module" + str(ListBModule.curselection()[0] + 1 ) + ".txt")
@@ -98,17 +92,18 @@ def load_Fill():
         boiteOutPut.insert(1.0,"Fichier correctement chargé :D","noerr")
 
 root = Tk()
-root.title("VoltaireTaMere 1.0.0")
-root.geometry('407x485')
+root.title("VoltaireTaMere 1.1.0")
+root.geometry('407x445')
 root.resizable(False, False)
 root.iconphoto(True,PhotoImage(file = "VoltaireTaMereIcoPNG.png"))
 root.configure(bg='#23272A')
 
 modules = StringVar()
-modules_Sup = "Module\ 1 Module\ 2 Module\ 3 Module\ 4 Module\ 5 Module\ 6 Module\ 7 Module\ 8 Module\ 9 Module\ 10"
+modules_Sup = "Module\ 1 Module\ 2 Module\ 3 Module\ 4 Module\ 5 Module\ 6 Module\ 7 Module\ 8 Module\ 9 Module\ 10 Test\ Blanc" #2-8
 modules_Exl = "Module\ 1 Module\ 2 Module\ 3 Module\ 4 Module\ 5 Module\ 6 Module\ 7 Module\ 8 Module\ 9 Module\ 10 Module\ 11 Module\ 12" #Verbes\ Pro\ I Verbes\ Pro\ II
 
 extra_PATH = StringVar()
+root.clipboard_append("")
 
 #----------------------------------------------------------------
 #ROOT MAIN LOOP
@@ -185,6 +180,7 @@ Label (root,
             font=10).place(x=10, y=230)
 
 boitePhrase = Text (root,
+                
                 height = 5,
                 width=48,
                 bg="#2C2F33",
@@ -192,20 +188,16 @@ boitePhrase = Text (root,
                 bd=0)
 boitePhrase.place(x=10, y=260)
 
-Button (root,
-                text="Vérifier",
-                command= test_Str,
-                fg="#ffffff",
-                bg="#8eb912",
-                font=15,
-                bd=0,).place(x=170, y=350)
-
 boiteOutPut = Text (root,
                 height = 5,
                 width=48,
                 bg="#23272A",
                 fg="#ffffff",
                 bd=0)
-boiteOutPut.place(x=10, y=390)
+boiteOutPut.place(x=10, y=350)
+
+boiteOutPut.tag_config("noerr",foreground = "#8eb912")
+boiteOutPut.tag_config("err",foreground = "#DC143C")
+root.after(500,auto_Work)
 
 root.mainloop()
